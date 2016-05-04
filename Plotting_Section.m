dc5461f4d25ec45b56d6b79774dfc71493174268
student <student@CIN-AriHP-PC02.cin.medizin.uni-tuebingen.de>
2016-05-04 09:57:27 +0200

    Verbesserung der Plots. Jetzt wird wirklich der Gain geplotet

--- Plotting_Section.m
+++ Plotting_Section.m
@@ -8,54 +8,79 @@
 %               tempfreq    = cycles/s
 %               spatfreq    = cycles/grad
 %               velocity    = tempfreq / 0.03333
+%               tempfreQ    = velocity * 0.03333
 %               Gain        = eye velocity / stimulus velocity
 
-% Velo gegen TempFreq -----------------------------------------------------
+% Fertig analysierte Daten laden (aus AlleFischeproDatensatz Skript)
+load('Saccaden_Analyse_CompleteWorkspace.mat')
+
+% Velo gegen TempVelo -----------------------------------------------------
 temp_freq = [0, 1.41, 45, 180, 11.25, 22.5, 90, 5.63, 2.81];
 VELO = [M_Velo_AllFish; temp_freq]';
 velo_sorted = sort(VELO);
+
+% Ploten
 figure(21)
 set(gcf,'Color',[1 1 1],'Position',[10 10 1000 500], 'Name', 'Velocity')
 subplot(2,1,1)
 plot(velo_sorted(:,2), velo_sorted(:,1),'-ko', 'LineWidth', 2)
 axis([0 200 0 max(velo_sorted(:,1))])
 set(gca,'TickDir','out', 'LineWidth',1.5)
-xlabel('Temporale Frequenz [cycles/s]')
+xlabel('Stimulusgeschwindigkeit [degree/s]')
 ylabel('Augengeschwindigkeit [degree/s]')
 box off
 subplot(2,1,2)
 semilogx(velo_sorted(:,2), velo_sorted(:,1),'-ko', 'LineWidth', 2)
 axis([0 300 0 max(velo_sorted(:,1))])
 set(gca,'TickDir','out', 'LineWidth',1.5)
-xlabel('Temporale Frequenz [cycles/s]')
+xlabel('Stimulusgeschwindigkeit [degree/s]')
 ylabel('Augengeschwindigkeit [degree/s]')
 box off
-print('finalfigs/VelocityOverTempFreq', '-dpng')
+print('finalfigs/VelocityOverStimVelo', '-dpng')
 
 % Gain gegen TempFreq -----------------------------------------------------
-gain = velo_sorted(2:end,1)./velo_sorted(2:end,2); % Phase 1 auslasen, da Stimvelo = 0 !
-stim_tempfreq = velo_sorted(2:end,2);
-% TempFreq in Velocity umwandeln (fuer Gain)
-stim_velo = round(stim_tempfreq / 0.03333, 1);
+stim_velo = velo_sorted(:,2);
+stim_tempfreq = round(stim_velo * 0.03333, 2);
+gain = velo_sorted(2:end,1)./stim_velo(2:end,1); % Phase 1 auslasen, da Stimvelo = 0 !
 
-
+% Ploten
 figure(22)
 set(gcf,'Color',[1 1 1],'Position',[10 10 1000 500], 'Name', 'Gain')
 subplot(2,1,1)
-plot(stim_tempfreq, gain, '-ko', 'LineWidth', 2)
-axis([0 200 0 1])
+plot(stim_tempfreq(2:end,1), gain, '-ko', 'LineWidth', 2)
+axis([0 6 0 1])
 set(gca,'TickDir','out', 'LineWidth',1.5)
 xlabel('Temporale Frequenz [cycles/s]')
 ylabel('Gain')
 box off
 subplot(2,1,2)
-semilogx(stim_tempfreq, gain, '-ko', 'LineWidth', 2)
-axis([0 300 0 1])
+semilogx(stim_tempfreq(2:end,1), gain, '-ko', 'LineWidth', 2)
+axis([0 6 0 1])
 set(gca,'TickDir','out', 'LineWidth',1.5)
 xlabel('Temporale Frequenz [cycles/s]')
 ylabel('Gain')
 box off
 print('finalfigs/GainOverTempFreq', '-dpng')
+
+% Velo gegen TempFreq -----------------------------------------------------
+% Ploten
+figure(23)
+set(gcf,'Color',[1 1 1],'Position',[10 10 1000 500], 'Name', 'Velocity')
+subplot(2,1,1)
+plot(stim_tempfreq(:,1), velo_sorted(:,1),'-ko', 'LineWidth', 2)
+axis([0 6 0 max(velo_sorted(:,1))])
+set(gca,'TickDir','out', 'LineWidth',1.5)
+xlabel('Temporale Frequenz [cycles/s]')
+ylabel('Augengeschwindigkeit [degree/s]')
+box off
+subplot(2,1,2)
+semilogx(stim_tempfreq(:,1), velo_sorted(:,1),'-ko', 'LineWidth', 2)
+axis([0 6 0 max(velo_sorted(:,1))])
+set(gca,'TickDir','out', 'LineWidth',1.5)
+xlabel('Temporale Frequenz [cycles/s]')
+ylabel('Augengeschwindigkeit [degree/s]')
+box off
+print('finalfigs/VelocityOverTempFreq', '-dpng')
 
 
 % %% Plot Raw Data
